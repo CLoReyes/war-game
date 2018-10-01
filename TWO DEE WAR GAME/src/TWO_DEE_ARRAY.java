4c437f16391bc9e4998fc32e6e8e58d4191b634e
carlos.reyes <carlos.reyes@MU-C1-18.mullen.pvt.k12.co.us>
2018-10-01 15:12:51 -0600

    start

---
+++ TWO DEE WAR GAME/src/TWO_DEE_ARRAY.java
@@ -0,0 +1,45 @@
+
+public class TWO_DEE_ARRAY
+	{
+
+		public static void main(String[] args)
+			{
+				war_map_display();
+				//
+			}
+
+		private static void war_map_display()
+			{
+				System.out.println("      1          2          3          4");
+				System.out.println("  _________  _________  _________  _________");
+				System.out.println("  |       |  |       |  |       |  |       |");
+				System.out.println("A |       |  |       |  |       |  |       |");
+				System.out.println("  |       |  |       |  |       |  |       |");
+				System.out.println("  ---------  ---------  ---------  ---------");
+				
+				System.out.println("  _________  _________  _________  _________");
+				System.out.println("  |       |  |       |  |       |  |       |");
+				System.out.println("B |       |  |       |  |       |  |       |");
+				System.out.println("  |       |  |       |  |       |  |       |");
+				System.out.println("  ---------  ---------  ---------  ---------");
+				
+				System.out.println("  _________  _________  _________  _________");
+				System.out.println("  |       |  |       |  |       |  |       |");
+				System.out.println("C |       |  |       |  |       |  |       |");
+				System.out.println("  |       |  |       |  |       |  |       |");
+				System.out.println("  ---------  ---------  ---------  ---------");
+				
+				System.out.println("  _________  _________  _________  _________");
+				System.out.println("  |       |  |       |  |       |  |       |");
+				System.out.println("D |       |  |       |  |       |  |       |");
+				System.out.println("  |       |  |       |  |       |  |       |");
+				System.out.println("  ---------  ---------  ---------  ---------");
+				
+				System.out.println("  _________  _________  _________  _________");
+				System.out.println("  |       |  |       |  |       |  |       |");
+				System.out.println("E |       |  |       |  |       |  |       |");
+				System.out.println("  |       |  |       |  |       |  |       |");
+				System.out.println("  ---------  ---------  ---------  ---------");
+			}
+
+	}
