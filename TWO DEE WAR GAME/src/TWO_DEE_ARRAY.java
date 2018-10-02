898ec692fff6f9975ca6ad72e6e7710cf953588b
carlos.reyes <carlos.reyes@MU-C1-18.mullen.pvt.k12.co.us>
2018-10-02 14:25:11 -0600

    map map amp

--- TWO DEE WAR GAME/src/TWO_DEE_ARRAY.java
+++ TWO DEE WAR GAME/src/TWO_DEE_ARRAY.java
@@ -1,45 +1,70 @@
 
 public class TWO_DEE_ARRAY
 	{
-
+		static int map [] [] = new int [8] [6];
+		
 		public static void main(String[] args)
 			{
 				war_map_display();
-				//
+				createMap();
+			}
+
+		private static void createMap()
+			{
+				
+				for (int row = 0; row < map.length; row++)
+					{
+						for (int col = 0; col < map[row].length; col++)
+							{
+								System.out.print(" ");
+							}
+						System.out.println();
+					}
 			}
 
 		private static void war_map_display()
 			{
-				System.out.println("      1          2          3          4");
-				System.out.println("  _________  _________  _________  _________");
-				System.out.println("  |       |  |       |  |       |  |       |");
-				System.out.println("A |       |  |       |  |       |  |       |");
-				System.out.println("  |       |  |       |  |       |  |       |");
-				System.out.println("  ---------  ---------  ---------  ---------");
+				System.out.println("      1          2          3          4          5          6");
+				System.out.println("  ---------  ---------  ---------  ---------  ---------  ---------");
+				System.out.println("  |       |  |       |  |       |  |       |  |       |  |       |");
+				System.out.println("1 |   " + map [0] [0] + "   |  |   " + map [0] [0] + "   |  |   " + map [0] [0] + "   |  |   " + map [0] [0] + "   |  |   " + map [0] [0] + "   |  |   " + map [0] [0] + "   |");
+				System.out.println("  |       |  |       |  |       |  |       |  |       |  |       |");
+				System.out.println("  ---------  ---------  ---------  ---------  ---------  ---------");
 				
-				System.out.println("  _________  _________  _________  _________");
-				System.out.println("  |       |  |       |  |       |  |       |");
-				System.out.println("B |       |  |       |  |       |  |       |");
-				System.out.println("  |       |  |       |  |       |  |       |");
-				System.out.println("  ---------  ---------  ---------  ---------");
+				System.out.println("  ---------  ---------  ---------  ---------  ---------  ---------");
+				System.out.println("  |       |  |       |  |       |  |       |  |       |  |       |");
+				System.out.println("2 |   " + map [0] [0] + "   |  |   " + map [0] [0] + "   |  |   " + map [0] [0] + "   |  |   " + map [0] [0] + "   |  |   " + map [0] [0] + "   |  |   " + map [0] [0] + "   |");				System.out.println("  |       |  |       |  |       |  |       |  |       |  |       |");
+				System.out.println("  ---------  ---------  ---------  ---------  ---------  ---------");
 				
-				System.out.println("  _________  _________  _________  _________");
-				System.out.println("  |       |  |       |  |       |  |       |");
-				System.out.println("C |       |  |       |  |       |  |       |");
-				System.out.println("  |       |  |       |  |       |  |       |");
-				System.out.println("  ---------  ---------  ---------  ---------");
+				System.out.println("  ---------  ---------  ---------  ---------  ---------  ---------");
+				System.out.println("  |       |  |       |  |       |  |       |  |       |  |       |");
+				System.out.println("3 |   " + map [0] [0] + "   |  |   " + map [0] [0] + "   |  |   " + map [0] [0] + "   |  |   " + map [0] [0] + "   |  |   " + map [0] [0] + "   |  |   " + map [0] [0] + "   |");				System.out.println("  |       |  |       |  |       |  |       |  |       |  |       |");
+				System.out.println("  ---------  ---------  ---------  ---------  ---------  ---------");
 				
-				System.out.println("  _________  _________  _________  _________");
-				System.out.println("  |       |  |       |  |       |  |       |");
-				System.out.println("D |       |  |       |  |       |  |       |");
-				System.out.println("  |       |  |       |  |       |  |       |");
-				System.out.println("  ---------  ---------  ---------  ---------");
+				System.out.println("  ---------  ---------  ---------  ---------  ---------  ---------");
+				System.out.println("  |       |  |       |  |       |  |       |  |       |  |       |");
+				System.out.println("4 |   " + map [0] [0] + "   |  |   " + map [0] [0] + "   |  |   " + map [0] [0] + "   |  |   " + map [0] [0] + "   |  |   " + map [0] [0] + "   |  |   " + map [0] [0] + "   |");				System.out.println("  |       |  |       |  |       |  |       |  |       |  |       |");
+				System.out.println("  ---------  ---------  ---------  ---------  ---------  ---------");
 				
-				System.out.println("  _________  _________  _________  _________");
-				System.out.println("  |       |  |       |  |       |  |       |");
-				System.out.println("E |       |  |       |  |       |  |       |");
-				System.out.println("  |       |  |       |  |       |  |       |");
-				System.out.println("  ---------  ---------  ---------  ---------");
+				System.out.println("  ---------  ---------  ---------  ---------  ---------  ---------");
+				System.out.println("  |       |  |       |  |       |  |       |  |       |  |       |");
+				System.out.println("5 |   " + map [0] [0] + "   |  |   " + map [0] [0] + "   |  |   " + map [0] [0] + "   |  |   " + map [0] [0] + "   |  |   " + map [0] [0] + "   |  |   " + map [0] [0] + "   |");				System.out.println("  |       |  |       |  |       |  |       |  |       |  |       |");
+				System.out.println("  ---------  ---------  ---------  ---------  ---------  ---------");
+				
+				System.out.println("  ---------  ---------  ---------  ---------  ---------  ---------");
+				System.out.println("  |       |  |       |  |       |  |       |  |       |  |       |");
+				System.out.println("6 |   " + map [0] [0] + "   |  |   " + map [0] [0] + "   |  |   " + map [0] [0] + "   |  |   " + map [0] [0] + "   |  |   " + map [0] [0] + "   |  |   " + map [0] [0] + "   |");				System.out.println("  |       |  |       |  |       |  |       |  |       |  |       |");
+				System.out.println("  ---------  ---------  ---------  ---------  ---------  ---------");
+				
+				System.out.println("  ---------  ---------  ---------  ---------  ---------  ---------");
+				System.out.println("  |       |  |       |  |       |  |       |  |       |  |       |");
+				System.out.println("7 |   " + map [0] [0] + "   |  |   " + map [0] [0] + "   |  |   " + map [0] [0] + "   |  |   " + map [0] [0] + "   |  |   " + map [0] [0] + "   |  |   " + map [0] [0] + "   |");				System.out.println("  |       |  |       |  |       |  |       |  |       |  |       |");
+				System.out.println("  ---------  ---------  ---------  ---------  ---------  ---------");
+				
+				System.out.println("  ---------  ---------  ---------  ---------  ---------  ---------");
+				System.out.println("  |       |  |       |  |       |  |       |  |       |  |       |");
+				System.out.println("8 |   " + map [0] [0] + "   |  |   " + map [0] [0] + "   |  |   " + map [0] [0] + "   |  |   " + map [0] [0] + "   |  |   " + map [0] [0] + "   |  |   " + map [0] [0] + "   |");				System.out.println("  |       |  |       |  |       |  |       |  |       |  |       |");
+				System.out.println("  ---------  ---------  ---------  ---------  ---------  ---------");
 			}
 
 	}
